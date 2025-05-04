63a160e0241bc071ec67506c51b09e3252c122c0
Shrutika Malve <113254896+ShrutikaM25@users.noreply.github.com>
2025-05-04 16:35:46 +0530

    Update Assignment_03.cpp

--- HPC/Assignment_03.cpp
+++ HPC/Assignment_03.cpp
@@ -1,54 +1,79 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 #include <omp.h>
-#include <limits>
 
 using namespace std;
 
 int main() {
-    const int N = 1000000;
-    int* arr = new int[N];
+    const int N = 1e7; // 10 million elements
+    vector<int> data(N);
 
-    // Initialize array
-    for (int i = 0; i < N; ++i)
-        arr[i] = rand() % 1000;
-
-    int min_val = INT_MAX;
-    int max_val = INT_MIN;
-    long long sum = 0;
-    double avg = 0.0;
-
-    double start = omp_get_wtime();
-
-    #pragma omp parallel for reduction(min:min_val)
+    // Initialize data
     for (int i = 0; i < N; ++i) {
-        if (arr[i] < min_val)
-            min_val = arr[i];
+        data[i] = i + 1;
     }
 
-    #pragma omp parallel for reduction(max:max_val)
+    // === Sequential Implementation ===
+    long long seq_sum = 0, seq_min = INT_MAX, seq_max = INT_MIN;
+    double seq_avg = 0.0;
+
+    double start_seq = omp_get_wtime();
+
     for (int i = 0; i < N; ++i) {
-        if (arr[i] > max_val)
-            max_val = arr[i];
+        seq_sum += data[i];
+        if (data[i] < seq_min) seq_min = data[i];
+        if (data[i] > seq_max) seq_max = data[i];
+    }
+    seq_avg = static_cast<double>(seq_sum) / N;
+
+    double end_seq = omp_get_wtime();
+    double duration_seq = (end_seq - start_seq) * 1000; // in ms
+
+    // === Parallel Implementation using OpenMP Reduction ===
+    long long par_sum = 0, par_min = INT_MAX, par_max = INT_MIN;
+    double par_avg = 0.0;
+
+    double start_par = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:par_sum)
+    for (int i = 0; i < N; ++i) {
+        par_sum += data[i];
     }
 
-    #pragma omp parallel for reduction(+:sum)
+    #pragma omp parallel for reduction(min:par_min)
     for (int i = 0; i < N; ++i) {
-        sum += arr[i];
+        if (data[i] < par_min) par_min = data[i];
     }
 
-    avg = static_cast<double>(sum) / N;
+    #pragma omp parallel for reduction(max:par_max)
+    for (int i = 0; i < N; ++i) {
+        if (data[i] > par_max) par_max = data[i];
+    }
 
-    double end = omp_get_wtime();
+    par_avg = static_cast<double>(par_sum) / N;
 
-    cout << "Min: " << min_val << endl;
-    cout << "Max: " << max_val << endl;
-    cout << "Sum: " << sum << endl;
-    cout << "Average: " << avg << endl;
-    cout << "Execution Time: " << (end - start) << " seconds" << endl;
+    double end_par = omp_get_wtime();
+    double duration_par = (end_par - start_par) * 1000; // in ms
 
-    delete[] arr;
+    // === Output Results ===
+    cout << "=== Sequential Results ===" << endl;
+    cout << "Sum     = " << seq_sum << endl;
+    cout << "Average = " << seq_avg << endl;
+    cout << "Min     = " << seq_min << endl;
+    cout << "Max     = " << seq_max << endl;
+    cout << "Time    = " << duration_seq << " ms" << endl;
+
+    cout << "\n=== Parallel Results ===" << endl;
+    cout << "Sum     = " << par_sum << endl;
+    cout << "Average = " << par_avg << endl;
+    cout << "Min     = " << par_min << endl;
+    cout << "Max     = " << par_max << endl;
+    cout << "Time    = " << duration_par << " ms" << endl;
+
     return 0;
 }
+
 
 // command to run this code: cd "d:\4th year sem 8\HPC\PRACTICALS\" ; if ($?) { g++ -fopenmp Assignment_02.cpp -o Assignment_02 } ; if ($?) { .\Assignment_02 }
 // OR
